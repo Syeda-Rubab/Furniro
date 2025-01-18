7fc882a69670e23bc1b7c471365bb5d3a7ae6ab9
Syeda-Rubab <161208396+Syeda-Rubab@users.noreply.github.com>
2025-01-18 12:36:50 +0500

    Delete lib directory

--- lib/storage/jotai/index.ts
+++
@@ -1,6 +0,0 @@
-import { atomWithStorage } from "jotai/utils";
-
-const cartAtom = atomWithStorage<IProduct[]>("CART_ITEMS", []);
-const billingAtom = atomWithStorage<null>("BILLING_ITEM", null);
-
-export { cartAtom, billingAtom };
